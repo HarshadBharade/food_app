b786aefa603dfb1dd6e9cf35da5e62fbcd576ddb
HarshadBharade <bharadeharshad106@gmail.com>
2025-05-12 00:13:40 +0530

    login signup ui complete

--- client/src/Auth/Login.tsx
+++ client/src/Auth/Login.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/components/ui/input"
 import { Separator } from "@radix-ui/react-separator";
 import { Loader2, LockKeyhole, Mail } from "lucide-react"
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState,type ChangeEvent,type ChangeEventHandler, type FormEvent } from "react";
 
 type LoginInputState = {
   email: string;
@@ -14,13 +14,21 @@ const Login = () => {
 const [input, setInput] = useState<LoginInputState>({
   email: "",
   password: "",
-})
+});
+const changeEventHandler = (e:ChangeEvent<HTMLInputElement>)=>{
+  const {name, value} = e.target;
+  setInput({...input,[name]:value});
+}
 
+const loginSubmitHandler = (e:FormEvent) => {
+  e.preventDefault();
+  console.log(input);
+}
 
   const loading = false;
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <form className="md:p-8 w-full max-w-md rounded-lg md:border border-gray-200 mx-4">
+      <form onSubmit={loginSubmitHandler} className="md:p-8 w-full max-w-md rounded-lg md:border border-gray-200 mx-4">
     <div className="mb-4">
       <h1 className="font-bold text-2xl">Patel Eats</h1>
     </div>
@@ -29,6 +37,9 @@ const [input, setInput] = useState<LoginInputState>({
     <Input 
     type="email"
     placeholder="Email"
+    name="email"
+    value={input.email}
+    onChange={changeEventHandler}
     className="pl-10 focus-visible:ring-1"/>
     <Mail className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none"></Mail>
     </div>
@@ -39,14 +50,17 @@ const [input, setInput] = useState<LoginInputState>({
     <Input 
     type="Password"
     placeholder="Password"
+    name= "password"
+    value={input.password}
+    onChange={changeEventHandler}
     className="pl-10 focus-visible:ring-1"/>
     <LockKeyhole className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none"></LockKeyhole>
     </div>
     </div>
     <div className="mb-10">
       {
-        loading ? <Button className="w-full bg-orange-400 hover:bg-orange-500"><Loader2 className="mr-2 h-4 w-4 animate-spin" />Please wait</Button> : (
-          <Button className="w-full bg-orange-400 hover:bg-orange-500">Login</Button>
+        loading ? <Button disabled className="w-full bg-orange-400 hover:bg-orange-500"><Loader2 className="mr-2 h-4 w-4 animate-spin" />Please wait</Button> : (
+          <Button type="submit" className="w-full bg-orange-400 hover:bg-orange-500">Login</Button>
           
         )
       }
--- client/src/Auth/Signup.tsx
+++ client/src/Auth/Signup.tsx
@@ -1,8 +1,106 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Separator } from "@radix-ui/react-separator";
+import { Contact2, Loader2, LockKeyhole, Mail, User2 } from "lucide-react"
+import { Link } from "react-router-dom";
+import { useState,type ChangeEvent,type ChangeEventHandler, type FormEvent } from "react";
+
+type SignupInputState = {
+  fullname: string;
+  email: string;
+  password: string;
+  contact: string;
+}
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
+const [input, setInput] = useState<SignupInputState>({
+  fullname:"",
+  email: "",
+  password: "",
+  contact: "",
+});
+const changeEventHandler = (e:ChangeEvent<HTMLInputElement>)=>{
+  const {name, value} = e.target;
+  setInput({...input,[name]:value});
 }
+
+const loginSubmitHandler = (e:FormEvent) => {
+  e.preventDefault();
+  console.log(input);
+}
+
+  const loading = false;
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <form onSubmit={loginSubmitHandler} className="md:p-8 w-full max-w-md rounded-lg md:border border-gray-200 mx-4">
+    <div className="mb-4">
+      <h1 className="font-bold text-2xl">Patel Eats</h1>
+    </div>
+    <div className="mb-4">
+    <div className="relative">
+    <Input 
+    type="text"
+    placeholder="Fullname"
+    name="fullname"
+    value={input.fullname}
+    onChange={changeEventHandler}
+    className="pl-10 focus-visible:ring-1"/>
+    <User2 className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none"></User2>
+    </div>
+    </div>
+    <div className="mb-4">
+    <div className="relative">
+    <Input 
+    type="email"
+    placeholder="Email"
+    name="email"
+    value={input.email}
+    onChange={changeEventHandler}
+    className="pl-10 focus-visible:ring-1"/>
+    <Mail className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none"></Mail>
+    </div>
+    </div>
+    
+    <div className="mb-4">
+    <div className="relative">
+    <Input 
+    type="Password"
+    placeholder="Password"
+    name= "password"
+    value={input.password}
+    onChange={changeEventHandler}
+    className="pl-10 focus-visible:ring-1"/>
+    <LockKeyhole className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none"></LockKeyhole>
+    </div>
+    </div>
+    <div className="mb-4">
+    <div className="relative">
+    <Input 
+    type="text"
+    placeholder="Contact"
+    name= "contact"
+    value={input.contact}
+    onChange={changeEventHandler}
+    className="pl-10 focus-visible:ring-1"/>
+    <Contact2 className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none"></Contact2>
+    </div>
+    </div>
+    <div className="mb-10">
+      {
+        loading ? <Button disabled className="w-full bg-orange-400 hover:bg-orange-500"><Loader2 className="mr-2 h-4 w-4 animate-spin" />Please wait</Button> : (
+          <Button type="submit" className="w-full bg-orange-400 hover:bg-orange-500">Signup</Button>
+          
+        )
+      }
+    </div>
+    <Separator />
+    <p className="mt-2">
+      Already have an account? {" "}
+      <Link to= "/signup" className="text-blue-500">Login</Link>
+    </p>
+      </form>
+    </div>
+  );
+};
 
 export default Signup
